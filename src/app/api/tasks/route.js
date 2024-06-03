958f8bdbe1b8443ec9c12f8b02435e4dda6e3317
David Fischer <fiscd@seas.upenn.edu>
2024-06-03 11:23:14 -0400

    connection mongoose

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,6 +1,7 @@
 import Task from "@/models/Task";
 import { dbConnect } from "@/utils/mongoose";
 import { NextResponse } from "next/server";
+const maxDuration = 60
 
 export async function GET() {
   await dbConnect();
@@ -10,6 +11,7 @@ export async function GET() {
 
 export async function POST(request) {
   try {
+    await dbConnect
     const body = await request.json();
     const newTask = new Task(body);
     const savedTask = await newTask.save();
