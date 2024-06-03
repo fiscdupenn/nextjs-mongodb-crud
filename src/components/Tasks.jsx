15b0bd4563b00d39088d59ce78b52852e586975b
David Fischer <fiscd@seas.upenn.edu>
2024-06-03 12:37:10 -0400

    added Tasks component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react'
+import TaskCard from './TaskCard'
+
+const Tasks = () => {
+    const [tasks, setTasks] = React.useState([])
+    React.useEffect(() => {
+        const loadTasks = async () => {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/tasks`, {
+                cache: 'no-store'
+            })
+            if (!res.ok) {
+                return {}
+            }
+            const data = await res.json()
+            setTasks(data)
+        }
+        loadTasks()
+    }, [])
+    return (
+        <div className='grid md:grid-cols-3 gap-2'>
+            {tasks.map((task) => (
+                <TaskCard task={task} key={task._id} />
+            ))}
+        </div>
+    )
+}
+
+export default Tasks
